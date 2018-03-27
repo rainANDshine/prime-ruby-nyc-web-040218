71887928e8487c2f5bee4624426fd92f95a12027
Shun Yao <ysmiracle@hotmail.com>
2018-03-27 06:24:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number <= 1
+  
+  for num in 2..number do
+    return true if number == num
+    return false if number % num == 0
+  end
+end
+
